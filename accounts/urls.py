1abe7c5d3af9056397780d252bee98c03aeef23b
taeho0888 <taeho0888@naver.com>
2022-08-28 07:31:43 +0900

    profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('signup/', signup, name= 'signup'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('edit/', edit, name='edit'),
+    path('profile/', edit, name='edit'),
     path('', home, name='home'),
 
 ]
